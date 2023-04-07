7231c2901e482521dda7e135cc8c907081d1bf3a
bilolxoja-botirov <bilolbotirov785@gmail.com>
2023-04-07 15:44:25 +0500

    Added header to index html and changed styles

---
+++ main.js
@@ -0,0 +1,12 @@
+function lang1(){
+    document.getElementById("lan1").style.backgroundColor='transparent';
+    document.getElementById("lan1").style.color='#A6A6A6';
+    document.getElementById("lan2").style.backgroundColor='#fff';
+    document.getElementById("lan2").style.color='#000';
+}
+function lang2(){
+    document.getElementById("lan2").style.backgroundColor='transparent';
+    document.getElementById("lan2").style.color='#A6A6A6';
+    document.getElementById("lan1").style.backgroundColor='#fff';
+    document.getElementById("lan1").style.color='#000';
+}
